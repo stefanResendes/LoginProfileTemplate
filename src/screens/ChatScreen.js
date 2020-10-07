1aec28fa266dde70bae65678866b08761b025d41
stefanResendes <stefan.resendes@gmail.com>
2020-10-07 16:06:27 -0400

    Added button to switch user to new chat room
    Start on chat room switching

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -8,21 +8,27 @@ import io from 'socket.io-client';
 
 const ChatScreen = ({ navigation }) => {
     const [messages, setMessages] = useState([]);
-    const socket = io('http://174.138.41.52:9090');
+    //const socket = io('http://174.138.41.52:9090');
+    const socket = io('http://192.168.1.3:9090');
 
     useEffect(() => {
       socket.connect();
+      socket.emit('username', global.User.email);
       socket.on('chat_return', msg => {
         setMessages(previousMessages => GiftedChat.append(previousMessages, msg));
       });
+      socket.on('is_online', ret => {
+        console.log(ret);
+      })
     }, []);
 
     const onSend = useCallback((messages = []) => {
-      console.log('Doing onSend');
       socket.emit('chat_message', messages);
     }, []);
 
-    
+    const _changeRoom = useCallback(() => {
+      socket.emit('switchRoom', 'room1');
+    }, []);
 
     const { width, height } = Dimensions.get('window');
     const actHeight = height - 200;
@@ -30,15 +36,19 @@ const ChatScreen = ({ navigation }) => {
 
     return (
       <Background>
-        <View style ={{ width: '100%', height: actHeight }}>
-            <GiftedChat
-                messages={messages}
-                onSend={(messages) => onSend(messages)}
-                user={{
-                _id: global.User.email,
-                }}
-                inverted= {inverted}
-            />
+        <View style={{ width: '100%', height: actHeight }}>
+          <Button mode="outlined" onPress={_changeRoom}>
+            Change to Group
+          </Button>
+          
+          <GiftedChat
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            user={{
+              _id: global.User.email,
+            }}
+            inverted={inverted}
+          />
         </View>
       </Background>
     );
